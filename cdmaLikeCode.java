78abca5600f45d82fc5a15873a12aad783692290
sheep94lion <sheep94lion@gmail.com>
2016-10-22 21:59:10 +0800

    on the way to re-construct code

--- cdmaLikeCode.java
+++ cdmaLikeCode.java
@@ -2,20 +2,49 @@ package com.zy.vlc;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+
+import com.backblaze.erasure.ReedSolomon;
 import org.json.*;
 
 /**
  * Created by yi on 16-10-14.
  */
 public class cdmaLikeCode {
+    private int length, height;
+    private int totalX, totalY, patternWidth;
+    private int[][] matrix;
+    private int n_bits;
+    private String message;
+    private int dataShards, parityShards, shardSize;
+
+    public void configure(String configureFilePath) throws IOException {
+        String configString;
+        JSONObject objConfig;
+        try {
+            configString = new String(Files.readAllBytes(Paths.get(configureFilePath)));
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+        objConfig = new JSONObject(configString);
+        length = objConfig.getInt("length");
+        height = objConfig.getInt("height");
+        patternWidth = objConfig.getInt("patternWidth");
+        totalX = objConfig.getInt("x");
+        totalY = objConfig.getInt("y");
+        n_bits = objConfig.getInt("n_bits");
+        message = objConfig.getString("message");
+        dataShards = objConfig.getInt("dataShards");
+        parityShards = objConfig.getInt("parityShards");
+        shardSize = objConfig.getInt("shardSize");
+    }
+
     public static void main(String[] args) {
-        System.out.println(System.getProperty("user.dir"));
         // get configuration
         String configString;
         try {
@@ -60,8 +89,12 @@ public class cdmaLikeCode {
         }
         //get byteArray
         String message = objConfig.getString("message");
+        int dataShards, parityShards, shardSize;
         byte[] byteArray;
-        byteArray = getByteArray(message);
+        dataShards = objConfig.getInt("dataShards");
+        parityShards = objConfig.getInt("parityShards");
+        shardSize = objConfig.getInt("shardSize");
+        byteArray = getByteArray(message, dataShards, parityShards, shardSize);
         // get the bit map for the whole image
         matrix = getTotalPattern(matrix, totalX, totalY, patternWidth, byteArray, n_bits, patterns);
         for (int x = 0; x < length; x++) {
@@ -91,7 +124,12 @@ public class cdmaLikeCode {
             e.printStackTrace();
         }
     }
-    private static int[][] getTotalPattern(int[][] matrix, int totalX, int totalY, int patternWidth, byte[] byteArray, int n_bits, int[][][] patterns) {
+
+    private void generateCodeImage(String outputFile) {
+
+    }
+
+    private int[][] getTotalPattern(int[][] matrix, int totalX, int totalY, int patternWidth, byte[] byteArray, int n_bits, int[][][] patterns) {
         if (n_bits == 2) {
             int index = 0;
             for (int i = 0; i < totalY; i++) {
@@ -119,8 +157,19 @@ public class cdmaLikeCode {
         }
         return matrix;
     }
-    private static byte[] getByteArray(String message) {
+
+    private byte[] getByteArray(String message, int dataShards, int parityShards, int shardSize) {
+        byte[][] shards = new byte[dataShards + parityShards][shardSize];
         byte[] byteArray = message.getBytes();
-        return byteArray;
+        for (int i = 0; i < dataShards; i++) {
+            System.arraycopy(byteArray, i * shardSize, shards[i], 0, shardSize);
+        }
+        ReedSolomon reedSolomon = ReedSolomon.create(dataShards, parityShards);
+        reedSolomon.encodeParity(shards, 0, shardSize);
+        byte[] resultBytes = new byte[(dataShards + parityShards) * shardSize];
+        for (int i = 0; i < (dataShards + parityShards); i++) {
+            System.arraycopy(shards[i], 0, resultBytes, i * shardSize, shardSize);
+        }
+        return resultBytes;
     }
 }
